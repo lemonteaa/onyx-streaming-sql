dcdfa4bfd4bc86926eabc58c9896cc86542ab156
lemonteaa <lemontea.Tom@gmail.com>
2022-01-04 22:02:59 +0000

    integrate window, aggregation, and trigger to prepare for join

--- onyx-stream-sql/src/onyx_stream_sql/core.clj
+++ onyx-stream-sql/src/onyx_stream_sql/core.clj
@@ -69,6 +69,29 @@
    :onyx/type :output
    :onyx/batch-size 20})
 
+(defn sum-init-fn [window]
+  0)
+
+(defn sum-aggregation-fn [window segment]
+  (if (= (:src segment) :person)
+    (let [k (-> segment :data :age)]
+      {:value k})
+    {:value 0}))
+
+;; Now just pull out the value and add it to the previous state
+(defn sum-application-fn [window state value]
+  (+ state (:value value)))
+
+
+;; sum aggregation referenced in the window definition.
+(def sum
+  {:aggregation/init sum-init-fn
+   :aggregation/create-state-update sum-aggregation-fn
+   :aggregation/apply-state-update sum-application-fn})
+
+(defn dump-window! [event window trigger x state]
+  (doall (map pp/pprint [event window trigger x state])))
+
 (defn run-process [sql segment]
   (let [ks (map #(keyword (name %)) (:select sql))]
     (select-keys (:data segment) ks)))
@@ -95,7 +118,16 @@
                             :onyx/params [:streamql/sql]
                             :onyx/batch-size 20})
                 true (conj (gen-output output-name)))
-     :lifecycles []}))
+     :lifecycles []
+     :windows [{:window/id :collect-segments
+                :window/task :proc
+                :window/type :global
+                :window/aggregation ::sum}]
+     :triggers [{:trigger/window-id :collect-segments
+                 :trigger/id :sync
+                 :trigger/on :onyx.triggers/segment
+                 :trigger/threshold [1 :elements]
+                 :trigger/emit ::dump-window!}]}))
 
 (runtest
  (compile-query test1)
@@ -103,3 +135,14 @@
   [:person {:age 67 :name "Peter" :other false :test 0}]
   [:person {:age 21 :name "Hackle" :other true}]
   [:person {:age 18 :name "Oliver" :other false :foo :bar}]])
+
+(runtest
+ (compile-query test2)
+ [[:person {:age 34 :name "Mary" :other true :test 123 :country "HKG"}]
+  [:country {:country-code "RUS" :country-name "Russia"}]
+  [:country {:country-code "IND" :country-name "India"}]
+  [:person {:age 67 :name "Peter" :other false :test 0 :country "RUS"}]
+  [:country {:country-code "JAP" :country-name "Japan"}]
+  [:person {:age 21 :name "Hackle" :other true :country "JAP"}]
+  [:person {:age 18 :name "Oliver" :other false :foo :bar :country "RUS"}]
+  [:country {:country-code "HKG" :country-name "Hong Kong"}]])
